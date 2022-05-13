0de25484ca7e5690970bc0f691784814784c70e4
Wouter-Weemaes-IT <78794324+Wouter-Weemaes-IT@users.noreply.github.com>
2022-05-14 01:02:15 +0200

    adding docker files to slave folder

--- testHandler.py
+++
@@ -1,9 +0,0 @@
-import os
-import subprocess
-
-def StartContainer(image,script,ip):
-    result = os.popen(f'sudo docker run {image} python3 ./{script} {ip}').read()
-    return result
-def StartQueue():
-
-    subprocess.call('python3 /home/azureuser/project/QueueHandler/queueHandler.py', shell=True)
