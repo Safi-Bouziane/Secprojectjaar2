5f4c3e6c18a30be1f8ac314c50e66cd4edbc0278
root <wouterweemaes@outlook.com>
2022-05-14 18:42:11 +0000

    adding some changes to the api

--- slave software/TestHandler.py
+++ slave software/TestHandler.py
@@ -1,15 +1,18 @@
 import os
 
-def startcontainers(ip,id,url,test1,test2,test3,test4,test5,test6):
+
+
+def StartTests(ip,id,url,test1,test2,test3,test4,test5,test6):
     if  test1:
-        os.popen(f'sudo docker run -d test1 python3 testhttpredirect.py {url} {id}')
+        os.popen(f'sudo docker run -d argusproof/argusproof_deltateam:test1 python3 testhttpredirect.py {url} {id}')
     if  test2:
-        os.popen(f'sudo docker run -d test2 python3 geoip.py {ip} {id}')
+        os.popen(f'sudo docker run -d argusproof/argusproof_deltateam:test2 python3 geoip.py {ip} {id}')
     if  test3:
-        os.popen(f'sudo docker run -d test3 python3 blacklistcheck.py {url} {id}')
+        os.popen(f'sudo docker run -d test3 argusproof/argusproof_deltateam:python3 blacklistcheck.py {url} {id}')
     if  test4:
-        os.popen(f'sudo docker run -d test4 python3 sslcheck.py {url} {id}')
+        os.popen(f'sudo docker run -d test4 argusproof/argusproof_deltateam:python3 sslcheck.py {url} {id}')
     if  test5:
-        os.popen(f'sudo docker run -d test5 python3 dnssec_check.py {url} {id}')
+        os.popen(f'sudo docker run -d argusproof/argusproof_deltateam:test5 python3 dnssec_check.py {url} {id}')
     if  test6:
-        os.popen(f'sudo docker run -d test6 python3 dnsipv6_check.py {url} {id}')
+        os.popen(f'sudo docker run -d argusproof/argusproof_deltateam:test6 python3 dnsipv6_check.py {url} {id}')
+    
--- slave software/mainapi.py
+++ slave software/mainapi.py
@@ -2,10 +2,13 @@ from fastapi import FastAPI,BackgroundTasks,File, UploadFile
 from pydantic import BaseModel
 from fastapi.staticfiles import StaticFiles
 from subprocess import Popen
+import psutil
+from TestHandler import StartTests
 
 class input(BaseModel):
     ip: str
     url: str
+    id: str
     test1: bool
     test2: bool
     test3: bool
@@ -15,11 +18,11 @@ class input(BaseModel):
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/cpuload")
 async def root():
-    return {"message": "Hello World"}
+    return psutil.cpu_percent()
 
 @app.post("/add/", status_code = 200)
 async def read_user_item(item: input):
-    StartTests(item.ip,item.url,item.test1,item.test2,item.test3,item.test4,item.test5,item.test6)
+    StartTests(item.ip,item.url,item.id,item.test1,item.test2,item.test3,item.test4,item.test5,item.test6)
     return "ok"
