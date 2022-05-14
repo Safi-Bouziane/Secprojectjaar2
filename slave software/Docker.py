77699b41953772b4c78275a30d4385c2b5ccd35b
Wouter-Weemaes-IT <78794324+Wouter-Weemaes-IT@users.noreply.github.com>
2022-05-14 22:09:37 +0200

    toevegen files voor slaves2

--- slave software/Docker.py
+++
@@ -1,3 +0,0 @@
-import docker
-client = docker.from_env()
-print(client.containers.run("popsv2", ["python3", "hello.py"]))
--- slave software/ip.py
+++
@@ -1,6 +0,0 @@
-import socket
-# create an INET, STREAMing socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-site = "www.facebook.com"
-
-print (socket.gethostbyname(site), 80)
