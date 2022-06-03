4aeb52d8610e35de1e3fcaf79f78d342ad67c65e
Safi-Bouziane <78704181+Safi-Bouziane@users.noreply.github.com>
2022-06-03 13:18:24 +0200

    Update mysqlInsert.py

--- Ansible/master_software/api/app/mysqlInsert.py
+++ Ansible/master_software/api/app/mysqlInsert.py
@@ -1,13 +1,15 @@
 
 import mysql.connector
-
-mydb = mysql.connector.connect(
- host="securityprojecthowsami.mysql.database.azure.com",
- user="safidesafi@securityprojecthowsami.mysql.database.azure.com",
- password="Honden120",
- database="securityproject"
-)
+def make_db_conn(mydb):
+    mydb = mysql.connector.connect(
+    host="securityprojecthowsami.mysql.database.azure.com",
+    user="safidesafi@securityprojecthowsami.mysql.database.azure.com",
+    password="Honden120",
+    database="securityproject"
+    )
+    return mydb
 def InsertIntoQueue(IP, RESULT, TEST1, TEST2, TEST3, TEST4, TEST5, TEST6):
+    mydb = make_db_conn(mydb)
     mycursor = mydb.cursor()
     sql = "INSERT INTO `queue`(IP,URL,TEST1,TEST2,TEST3,TEST4,TEST5,TEST6) values(%s,%s,%s,%s,%s,%s,%s,%s);"
     val = (IP, RESULT, TEST1, TEST2, TEST3, TEST4, TEST5, TEST6)
