adc6d43c12c5b5b40f722b6e2b80c10e203b4571
Wouter-Weemaes-IT <78794324+Wouter-Weemaes-IT@users.noreply.github.com>
2022-06-12 19:51:38 +0200

    final push

--- Ansible/run.sh
+++ Ansible/run.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 cd /home/master/master_software/ResultHandler/ && nohup uvicorn ResultHandler:app --host 0.0.0.0 --reload --port 9000 &
-cd /home/master/master_software/api/app/ && nohup uvicorn main:app --host 0.0.0.0 --reload --port 8000 &
-python3 /home/master/slave_software/QueueHandler/master.py
+cd /home/master/master_software/api/app/ && nohup uvicorn main:app --host 0.0.0.0 --reload --port 8000
+
 
 
 #/***************************************************************
