2dbbb3658c7577cf9903c434b9c8f71080fa420f
Wouter-Weemaes-IT <78794324+Wouter-Weemaes-IT@users.noreply.github.com>
2022-06-12 13:27:30 +0200

    adding bug fixes to slave files

---
+++ Ansible/runslave.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+cd /home/master/slave_software/ResultHandler/ && nohup uvicorn ResultHandler:app --host 0.0.0.0 --reload --port 9000 &
+cd /home/master/slave_software/api/app/ && nohup uvicorn main:app --host 0.0.0.0 --reload --port 8000 &
+python3 /home/master/slave_software/QueueHandler/master.py
+
+
+#/***************************************************************
+#*
+#* Copyright (Wouter Weemaes, Safi Bouziane, Kamil Grielens, Robbe Willeme) - All rights reserved. 
+#*
+#* Unauthorized use, copy, modify, merge, publish, distribute, sublicense, 
+#* and/or sell any parts of the software/source code is strictly prohibited.
+#* Proprietary and confidential
+#* License: No license. 
+#* Written by: (Wouter Weemaes) (wouterweemaes@outlook.com), (06/2022)
+#*
+#****************************************************************/
