888a8ffc1ff79a17096b97518af9b24953ad411c
Robbe Willeme <robbe.willeme@student.ap.be>
2022-06-12 11:31:44 +0200

    updated mysqlinsert.py

--- Ansible/master_software/api/app/mysqlInsert.py
+++ Ansible/master_software/api/app/mysqlInsert.py
@@ -8,11 +8,11 @@ def make_db_conn(mydb):
     database="securityproject"
     )
     return mydb
-def InsertIntoQueue(IP, RESULT, TEST1, TEST2, TEST3, TEST4, TEST5, TEST6):
+def InsertIntoQueue(IP, RESULT, TEST1, TEST2, TEST3, TEST4, TEST5, TEST6, TEST7, TEST8):
     mydb = make_db_conn(mydb)
     mycursor = mydb.cursor()
-    sql = "INSERT INTO `queue`(IP,URL,TEST1,TEST2,TEST3,TEST4,TEST5,TEST6) values(%s,%s,%s,%s,%s,%s,%s,%s);"
-    val = (IP, RESULT, TEST1, TEST2, TEST3, TEST4, TEST5, TEST6)
+    sql = "INSERT INTO `queue`(IP,URL,TEST1,TEST2,TEST3,TEST4,TEST5,TEST6, TEST7, TEST8) values(%s,%s,%s,%s,%s,%s,%s,%s);"
+    val = (IP, RESULT, TEST1, TEST2, TEST3, TEST4, TEST5, TEST6, TEST7, TEST8)
     mycursor.execute(sql, val)
 
     mydb.commit()
